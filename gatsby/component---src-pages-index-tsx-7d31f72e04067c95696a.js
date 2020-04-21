9aadfe064eaff752dc4e40deb90f8e237c9c2c17
esrenll <esrenll@gmail.com>
2020-04-22 02:26:05 +0900

    update gatsby

---
+++ gatsby/component---src-pages-index-tsx-7d31f72e04067c95696a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{QeBL:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a),c=t("9Dj+"),o=t("H8eV");n.default=function(){return l.a.createElement(c.a,null,l.a.createElement(o.a,{title:"Home"}),l.a.createElement("h1",null,"home"))}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-7d31f72e04067c95696a.js.map
